1f63b96325430a8bb821b3f400a130d632843fb0
plneappl <simon.wegendt@stundent.uni-tuebingen.de>
2015-04-30 21:54:07 +0200

    preliminary type system for grammars and transformations and the concrete and abstract grammar from algorithm/master

--- Transform.scala
+++ Transform.scala
@@ -1,193 +1,54 @@
-import java.util.regex.Pattern
-
 object Transform {
+	trait GrammarAtom
+	case class Nonterminal(sym: Symbol) extends GrammarAtom
+	case class Terminal(sym: String) extends GrammarAtom
+	case object IntegerTerminal extends GrammarAtom
 	
-	//TransformationRules are 1. a Pattern matching a rule to be transformed 2. output patterns, that create rules.
-	case class TransformationRule(fromPattern: String, toPatterns: List[String])
-	//A Transformation Object is a regex for pattern matching, format strings for outputting new rules,
-	//and the number of new Nonterminals beeing introduced.
-	case class TransformationObject(fromRegex: String, toFStrings: List[String], newNTsCount: Int)
-	//TokenIndices is a Map mapping Nonterminals to their first appearance in a pattern
-	//therefore "A -> BDC" will make a map like Map(A -> 1, B -> 2, D -> 3, C -> 4)
-	type TokenIndices = Map[String, Int]
-	//This is a rule to be transformed; we want to factor out the 'B'
-	val in: String = "A -> BC | BD"
+	case class GrammarRule(lhs: Nonterminal, rhs: List[GrammarAtom])
 	
-	//Transformation rules see everything separated by spaces as tokens. Nonterminals are anything starting with a capital letter.
-	//New nonterminals may be introduced simply by using them, the next lexicographically available nt will be used.
-	val factor = TransformationRule("N1 -> N2 N3 | N2 N4", List("N1 -> N2 N5", "N5 -> N3 | N4"))
-	//Output shoud be: A -> B B'; B' -> C | D
-
-	//buildTransformationRules takes a TransformationRule and a grammatical rule
-	def buildTransformationRule(transformationRule: TransformationRule)(rule: String):TransformationObject = {
-		//build the in-pattern-matchers
-		var (inRegex, knownTokens) = buildInRegex(transformationRule.fromPattern)
-
-		var knownTokensCount = knownTokens.size
-		var result = List[String]()
-
-		//build the out-patterns
-		for(toPattern <- transformationRule.toPatterns){
-			val (outFString, knownTokens2) = buildOutFormatString(knownTokens)(toPattern)
-			//update known nonterminals
-			knownTokens = knownTokens2
-			result = result :+ outFString
-		}
-		//tokens introduced by the out-patterns
-		var newTokensCount = knownTokens.size - knownTokensCount 
-		TransformationObject(inRegex, result, newTokensCount)
-	}
-
-	//increment on Strings over {\epsilon, A, B, C, ..., Z}
-	def nextLexicographic(nts: String): String = {
-		// \epsilon+1 == "A"
-		if(nts.length == 0) "A"
-		else {
-			var res = ""
-			var incrementing = true
-			for(c <- nts.reverseIterator){
-				//carry
-				if(incrementing && c == 'Z') {
-					res = 'A' + res
-				}
-				//increment
-				else if(incrementing){
-					res = ((c + 1).asInstanceOf[Char]) + res
-					incrementing = false
-				}
-				//copy
-				else{
-					res = c + res
-				}
-			}
-			res
-		}
-	}
-
-	//transform a grammar rule
-	def applyTransformationRule(tfo: TransformationObject)(in: String): List[String] = {
-		var result = List[String]()
-		println()
-		println("Pattern: " + tfo.fromRegex)
-		println("Trying to match: " + in)
-
-		//this is the "complicated" part: finding out how to get a list of matches
-		var vars = tfo.fromRegex.r.findFirstMatchIn(in).get.subgroups
-
-		//add new nonterminals, starting at "A"
-		var nextNonterminal = "A"
-		for(i <- 0 until tfo.newNTsCount){
-			//just try the next lexicographical one until it's a new one
-			while(vars.contains(nextNonterminal)) {
-				nextNonterminal = nextLexicographic(nextNonterminal)
-			}
-			vars = vars :+ nextNonterminal 
-		}
-		
-		println("Vars: " + vars)	
-
-		tfo.toFStrings.foreach(println)
-		tfo.toFStrings.foreach(x => {
-			//apply the format strings to get the transformed rules
-			result = result :+ x.format(vars:_*) 
-		})
-		result
-	}
-
-	//taking a out-rule and a map of known nonterminals, build an out format string
-	def buildOutFormatString(tokIndices: TokenIndices)(rule: String): (String, TokenIndices) = {
-		//Tokens are separated by spaces
-		val tokens = rule.split(" ")
-		var result = ""
-		//Nonterminals start with a capital letter
-		val nonTerminal = "[A-Z].*".r
-		//read/write
-		var known = tokIndices
-		var ntCount = known.size + 1
-		for(tok <- tokens) {
-			tok match {
-				//replace nonterminals by the index in the ntMap, add them if unknown
-				case nonTerminal(_*) => {
-					if(known.getOrElse(tok, 0) == 0) {
-						//new nonterminal, use it and return in the Map
-						known += (tok -> ntCount)
-						ntCount += 1
-					}
-					
-					result += "%" + known(tok) + "$s"
-					
-				}
-				//terminals are not transformed
-				case _ => {
-					result += tok
-				}
-			}
-		}
-		//return the map, so other format strings know about new nonterminals
-		(result, known)
-	}
-
-	//this works quite the same as buildOutFormatString, differences are commented
-	//also adds arbitrary space between tokens, just to be safe
-	def buildInRegex(rule: String): (String, TokenIndices) = {
-		val tokens = rule.split(" ")
-		var known = Map[String, Int]()
-		var result = ""
-		val nonTerminal = "[A-Z].*".r
-		var subExprCount = known.size + 1
-		for(tok <- tokens) {
-			tok match {
-				case nonTerminal(_*) => {
-					if(known.getOrElse(tok, 0) == 0) {
-						
-						known += (tok -> subExprCount)
-						subExprCount += 1
-						
-						//difference: first occurences of nonterminals are used to parse them, later ones will get backreferenced
-						result += "([A-Z][a-z]*)\\s*"
-					}
-					else {
-						//backreference 
-						result +=  "\\" + known(tok)
-					}
-				}
-				case _ => {
-					//quote every non-nonterminal
-					result += Pattern.quote(tok) + "\\s*"
-				}
-			}	
-		}
-		(result, known)
-	}
-
-	trait InformationPath
-	case class Rename(from:(String, Int), to:(String, Int)) extends InformationPath
-	case class Create(from:(String, Int), to:((String, Int, Int), (String, Int))) extends InformationPath
-	case class Delete(from:(String, Int), to:String) extends InformationPath
-	case class ProductionRuleIdentifier(nonterminal: String, index: Int)
-	case class ProductionRuleTransform(from: ProductionRuleIdentifier, to: ProductionRuleIdentifier)
-	case class FlowFragment(transform: ProductionRuleTransform, informationPaths: List[InformationPath])
-	type InformationFlow = List[FlowFragment]
-
-	//wrapper for easier use
-	def transformGrammarRule(factor: TransformationRule)(in: String): (List[String], InformationFlow) = {
-		var to = buildTransformationRule(factor)(in)
-		(applyTransformationRule(to)(in), List(
-			FlowFragment(ProductionRuleTransform(ProductionRuleIdentifier("A", 1), ProductionRuleIdentifier("A", 1)), List(Rename(("B", 1), ("B", 1)), Create(("C", 2), (("E", 2, 2), ("C", 3))))),
-			FlowFragment(ProductionRuleTransform(ProductionRuleIdentifier("A", 2), ProductionRuleIdentifier("A", 1)), List(Rename(("B", 3), ("B", 1)), Create(("D", 4), (("E", 3, 2), ("D", 4)))))
-			)
+	case class Grammar(start: Nonterminal, rules: List[GrammarRule])
+	
+	val c = Nonterminal('C)
+	val a = Nonterminal('A)
+	val s = Nonterminal('S)
+	val f = Nonterminal('F)
+	
+	val leftBrace  = Terminal("(")
+	val rightBrace = Terminal(")")
+	val plus       = Terminal("+")
+	val mul			   = Terminal("*")
+	
+	val rules1 = List(
+		GrammarRule(c, List(c, plus, s)),
+		GrammarRule(c, List(s)),
+		GrammarRule(s, List(s, mul, f)),
+		GrammarRule(s, List(f)),
+		GrammarRule(f, List(IntegerTerminal)),
+		GrammarRule(f, List(leftBrace, c, rightBrace))
 		)
-		
-	}
 	
-
-	def main(args: Array[String]): Unit = {
-		var transformed = transformGrammarRule(factor)(in)
-		println()
-		println("In: ")
-		println(in)
-		println()
-		println("Transformed: ")
-		println(transformed)
+	val g1 = Grammar(c, rules1)
+	
+	val rules2 = List(
+		GrammarRule(a, List(a, plus, a)),
+		GrammarRule(a, List(a, mul, a)),
+		GrammarRule(a, List(IntegerTerminal))
+		)
+	val g2 = Grammar(a, rules2)
+	
+	trait TransformerAtom
+	case class NonterminalMatcher(id: Int) extends TransformerAtom
+	case class TerminalMatcher(matches: String) extends TransformerAtom
+	case object RightArrow extends TransformerAtom
+	case class TransformerOr(either: TransformerSequence, or: TransformerSequence) extends TransformerAtom
+	
+	case class TransformerSequence(lhs: NonterminalMatcher, rhs: List[TransformerAtom])
+	case class TransformerRule(from: TransformerSequence, to: TransformerSequence)
+	type TransformerRules = List[TransformerRule]
+	
+	
+	def main(args: Array[String]) = {
+		println(g1)
+		println(g2)
 	}
 }

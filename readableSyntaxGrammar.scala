d170419b9a39fb4f6a7d7571f654a42c7eb5a768
plneappl <simon.wegendt@stundent.uni-tuebingen.de>
2015-06-09 12:17:51 +0200

    Fixed some errors in parsing the .tr s

--- readableSyntaxGrammar.scala
+++ readableSyntaxGrammar.scala
@@ -1,6 +1,7 @@
 object ReadableSyntaxGrammar {
 	import Grammar._
 	import Transform._
+	import sext._
 	
 	val List(start, doc, ruleDef, inPart, outPart, sequencePart, ruleMatchers, ruleMatcher, rhs, rhsAtom) = 
 		List('start, 'doc, 'ruleDef, 'inPart, 'outPart, 'sequencePart, 'ruleMatchers, 'ruleMatcher, 'rhs, 'rhsAtom).map(Nonterminal)
@@ -12,7 +13,7 @@ object ReadableSyntaxGrammar {
 	val t_rightArrow  = Terminal("""->"""                  )
 	val t_colon       = Terminal(""":"""                   )
 	val t_pipe        = Terminal("""|"""                   )
-	val nt            = Regex(   """[A-Z][A-Za-z]*"""      )
+	val nt            = Regex(   """[A-Z][A-Za-z0-9]*"""   )
 	val term          = Regex(   """[^A-Z|<:\s][^|<:\s]*""")
 	val t_int         = Terminal("""<int>"""               )
 	val t_optSpace    = Regex(   """ *"""                  )
@@ -41,24 +42,27 @@ object ReadableSyntaxGrammar {
 		GrammarRule(rhsAtom     , List(t_int, t_colon, IntegerTerminal                          ), 0),                                                            
 		GrammarRule(rhsAtom     , List(t_int                                                    ), 0),                                                                                                                                           
 		GrammarRule(rhsAtom     , List(t_pipe                                                   ), 0)                                                    
-	) zip (Stream from 1)).map(x => x match{ case (GrammarRule(a, b, _), c) => GrammarRule(a, b, c)})          
+	) zip (Stream from 1)).map(x => x match { case (GrammarRule(a, b, _), c) => GrammarRule(a, b, c)})          
 	
 	def grammar = Grammar(start, rules)
 	
 	def parseReadableSyntax = parseWithGrammar(grammar) _
 	
-	type SyntaxTreeRecurser[A] = SyntaxTree => Option[A]
+	type SyntaxTreeRecurser[A] = List[SyntaxTree] => Option[A]
 	
 	def operateOnRRTree[A](s: Symbol)(f: SyntaxTreeRecurser[A])(t: SyntaxTree): List[A] = t match {
-		case Branch(sym, list) => list.map(f).flatten ++ list.map(operateOnRRTree(s)(f)).flatten
+		case Branch(_, list) => list.map(x => x match { 
+			case Branch(s1, list1) if(s1 == s) => f(list1)
+			case _ => None
+		}).flatten ++ list.map(operateOnRRTree(s)(f)).flatten
 		case _ => List()
 	}
 		
 	def docToTransformerRules(t: SyntaxTree): GrammarTransformer = t match {
 		case Branch('start, List(_, _, _, LeafString(nt), _, doc)) => { 
 			GrammarTransformer(
-				NonterminalMatcher(nt, -1), 
-				splitRules(operateOnRRTree('doc)(ruleDefsToTransformerRules)(doc))
+				NonterminalMatcher(nt, 0), 
+				splitRules(operateOnRRTree('ruleDef)(ruleDefsToTransformerRules)(doc))
 			)
 		}
 	}
@@ -67,10 +71,10 @@ object ReadableSyntaxGrammar {
 			x => TransformerRule(x.from.map(splitMatcher).flatten, x.to.map(splitMatcher).flatten)
 	)
 	def splitMatcher(t: GrammarRuleMatcher): List[GrammarRuleMatcher] = {
-		var (t1, t2) = t.rhs.span(x => x != TerminalMatcher("|", -1))
+		var (t1, t2) = t.rhs.span(x => !x.isInstanceOf[TerminalMatcher] || x.asInstanceOf[TerminalMatcher].matches != "|")
 		var res = List(GrammarRuleMatcher(t.lhs, t1))
 		while(t2 != Nil){
-			var (t11, t21) = t2.tail.span(x => x != TerminalMatcher("|", -1))
+			var (t11, t21) = t2.tail.span(x => !x.isInstanceOf[TerminalMatcher] || x.asInstanceOf[TerminalMatcher].matches != "|")
 			t2 = t21
 			res = GrammarRuleMatcher(t.lhs, t11) :: res
 		}
@@ -78,21 +82,31 @@ object ReadableSyntaxGrammar {
 	}
 	
 	def ruleDefsToTransformerRules: SyntaxTreeRecurser[TransformerRule] = t => t match {
-		case Branch('ruleDef, List(in, out)) => {
-			Some(TransformerRule(
-				operateOnRRTree('ruleMatchers)(ruleMatcherToGrammarRuleMatcher)(in),
-				operateOnRRTree('ruleMatchers)(ruleMatcherToGrammarRuleMatcher)(out)	
-			))
+		case List(in, out) => {
+			var res = TransformerRule(
+				operateOnRRTree('ruleMatcher)(ruleMatcherToGrammarRuleMatcher)(in),
+				operateOnRRTree('ruleMatcher)(ruleMatcherToGrammarRuleMatcher)(out)	
+			)
+			if(out.asInstanceOf[Branch].childs(0).asInstanceOf[LeafString].str.startsWith("seq:")){
+				val usedTags = (res.from ++ res.to).map(r => getUsedTags(r.rhs)).flatten
+				res = TransformerRule(
+					res.from.map(rule => rule.copy(rhs = (rule.rhs zip (1 to (rule.rhs.length + usedTags.length)).diff(usedTags)).map(x => if(x._1.tag < 0) x._1.copy(x._2) else x._1))),
+					  res.to.map(rule => rule.copy(rhs = (rule.rhs zip (1 to (rule.rhs.length + usedTags.length)).diff(usedTags)).map(x => if(x._1.tag < 0) x._1.copy(x._2) else x._1)))
+				)
+			}
+			Some(res)
 		}
 		case _ => None
 	}
 	
+	val getUsedTags: List[TransformerAtom] => List[Int] = x => x.map(y => {if(y.tag > 0) Some(y.tag) else None}).flatten
+	
 	def ruleMatcherToGrammarRuleMatcher: SyntaxTreeRecurser[GrammarRuleMatcher] = t => { 
 		t match {
-			case Branch('ruleMatcher, List(nt, _, _, _, rhs, _)) => {
+			case List(nt, _, _, _, rhs, _) => {
 				Some(GrammarRuleMatcher(
-					NonterminalMatcher(nt.asInstanceOf[LeafString].str, -1), 
-					operateOnRRTree('rhs)(rhsAtomToTransformerAtom)(rhs)
+					NonterminalMatcher(nt.asInstanceOf[LeafString].str, 0), 
+					operateOnRRTree('rhsAtom)(rhsAtomToTransformerAtom)(rhs)
 				))
 			}
 			case _ => None
@@ -100,13 +114,13 @@ object ReadableSyntaxGrammar {
 	}
 	
 	def rhsAtomToTransformerAtom: SyntaxTreeRecurser[TransformerAtom] = t => t match {
-		case Branch('rhsAtom, List(LeafString(t1), _, LeafInteger(i))) => LeafStringToTransformerAtom(i)(t1)
-		case Branch('rhsAtom, List(LeafString(t1))) => LeafStringToTransformerAtom(-1)(t1)
+		case List(LeafString(t1), _, LeafInteger(i)) => LeafStringToTransformerAtom( i)(t1)
+		case List(LeafString(t1))                    => LeafStringToTransformerAtom(-1)(t1)
 		case _ => None
 	}
 	
 	def LeafStringToTransformerAtom(i: Int): String => Option[TransformerAtom] = t => {
-		val ntpattern = """([A-Z][A-Za-z]*)""".r
+		val ntpattern = """([A-Z][A-Za-z0-9]*)""".r
 		val termpattern = """([^A-Z|<:\s][^|<:\s]*)""".r
 		t match {
 			case ntpattern(nt) => Some(NonterminalMatcher(nt, i))
@@ -115,5 +129,14 @@ object ReadableSyntaxGrammar {
 			case "|" => Some(TerminalMatcher("|", -1))
 			case _ => None
 		}
+	}
+	
+	def getGrammarTransformer(path: String): GrammarTransformer = {
+		val source = scala.io.Source.fromFile(path)
+		val tr = source.mkString
+		source.close
+		val tr2 = (parseWithGrammar(grammar)(tr))
+		//println(tr2.treeString)
+		docToTransformerRules(tr2)
 	}
 }
